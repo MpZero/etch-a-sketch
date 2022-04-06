7f4aacedc280a7339a6ebb7d65968e224b85645b
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-04-06 14:39:52 -0300

    add grid of square divs

---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log("h")
+const container = document.querySelector("#container");
+
+const content = document.querySelector("div");
+
+//  function a(){
+  for (let i = 0; i<257; i++) {
+  const div = document.createElement("div");
+  div.setAttribute("id", "divGrid");
+  container.appendChild(div);
+}
+
+// content.forEach((div) => {
+// div.addEventListener("hover", () => {
+//  console.log("hey")
+// })
+// });
+
+//  }
+//  a()
+// container.style.backgroundColor = "blue"
+
+// content.style.backgroundColor = "blue
+// content.textContent = "banana"
