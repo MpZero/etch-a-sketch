efc088a593edac9aa9a16886b7727a0f4dd545a8
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-04-09 16:18:22 -0300

    add button that clears the canvas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
-
 const content = document.querySelector("div");
+const btnEl = document.querySelector("#btn-el");
 
 for (let i = 0; i<256; i++) {
   const div = document.createElement("div");
@@ -12,15 +12,44 @@ const divGrid = document.querySelectorAll("#divGrid");
 //for each div that the mouse passes over it sets a new class and calls a function to "paint" the divs with new class
 divGrid.forEach((div) => {
   div.addEventListener('mousemove', () => {
-    div.setAttribute("class", "paintedDiv");
-    changebackground(div)
+    // div.setAttribute("class", "paintedDiv");
+    changebackground(div);
   });
 });
 
-function changebackground(e) {
-e.style.backgroundColor = "black";
+function changebackground(div) {
+div.style.backgroundColor = "black";
 }
 
-  
 
+btnEl.addEventListener('click', () => {
+  clearCanvas()
+  userInput()
+});
+
+//clear the canvas 
+function clearCanvas() {
+  divGrid.forEach((div) => {
+      div.style.backgroundColor = ""
+    });
+}
+
+//get the desired grid size
+  function userInput() {
+    removeDiv()
+    // let result = window.prompt()
+    // for (let i = 0; i<result; i++) {
+    //   const div = document.createElement("div");
+    //   div.setAttribute("id", "divGrid");
+    //   container.appendChild(div);
+    }
+  // }
+  function removeDiv () {
+    let eliminar = document.querySelectorAll("#divGrid");
+    container.removeChild(eliminar)
+  }
+  
+//maybe delete all divs before creating a new canvas
+//Maybe toggle paintedDiv class off later
+//userinput() for loop maybe
 
