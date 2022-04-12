dabf90a0314a798055653dfd1574298c4cc917b5
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-04-12 15:42:08 -0300

    make the clear canvas button delete the old grid of divs and create a new one with new size if the user so chooses

--- index.js
+++ index.js
@@ -1,71 +1,72 @@
 const container = document.querySelector("#container");
 const content = document.querySelector("div");
 const btnEl = document.querySelector("#btn-el");
-let gridSize = 16
-// let rowSize =
-container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
-container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
-//set the quantity of squares per side
+let pixels = 16;
 
-//set the quantity of pixels
-let pixels = 256
-for (let i = 0; i < pixels; i++) {
-  const div = document.createElement("div");
-  div.setAttribute("id", "divGrid");
-  container.appendChild(div);
+//renders the game
+function renderGame() {
+  createGrid();
+}
+
+//creates the grid by reading the number of squares(pixels), rows and columns
+function createGrid() {
+  pixelDivs();
+  container.style.gridTemplateColumns = `repeat(${pixels}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${pixels}, 1fr)`;
+  //set the quantity of squares per side
+}
+
+//set the quantity of divs(pixels) and adds event to paint them
+function pixelDivs() {
+  for (let i = 0; i < pixels * pixels; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("id", "divGrid");
+    container.appendChild(div);
+    div.addEventListener('mousemove', () => {
+      // div.setAttribute("class", "paintedDiv");
+      changebackground(div);
+    });
+  }
 }
 const divGrid = document.querySelectorAll("#divGrid");
 
-
-//for each div that the mouse passes over it sets a new class and calls a function to "paint" the divs with new class
-divGrid.forEach((div) => {
-  div.addEventListener('mousemove', () => {
-    // div.setAttribute("class", "paintedDiv");
-    changebackground(div);
-  });
-});
-
+//paints divs black
 function changebackground(div) {
   div.style.backgroundColor = "black";
 }
 
-
+//first clears canvas, then asks the user for new grid
 btnEl.addEventListener('click', () => {
   clearCanvas()
   userInput()
 });
 
-//clear the canvas 
+//clear the canvas (by making it color white)
 function clearCanvas() {
   divGrid.forEach((div) => {
     div.style.backgroundColor = ""
   });
 }
 
-//get the desired grid size
+//first removes old grid, the get the desired grid size and renders the game with new values
 function userInput() {
-  removeDiv()
-  gridSize = window.prompt("Write a number between 10 and 100", 16)
-
-}
-// for (let i = 0; i<result; i++) {
-//   const div = document.createElement("div");
-//   div.setAttribute("id", "divGrid");
-//   container.appendChild(div);
-//     }
-//   // }
-//removes old grid
-function removeDiv() {
+  removeGrid()
+  pixels = window.prompt("Write a number between 10 and 100", 16)
+  if (pixels <= 9 || pixels >= 101) {
+    alert("Error")
+    return userInput()
+  } else {
+    renderGame()
+  }
   
-  let eliminar = document.getElementById("#divGrid");
-  // container.removeNode
-
-  ///use .remove  maybe
-  // container.removeChild(eliminar)
-  // container.remove(eliminar)
 }
 
-//maybe delete all divs before creating a new canvas
-//Maybe toggle paintedDiv class off later
-//userinput() for loop maybe
+//removes old grid
+function removeGrid() {
+  let squares = container.querySelectorAll("div")
+  squares.forEach((div) => div.remove());
+}
+
+renderGame()
+
 
